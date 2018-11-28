ccd3f42cfd6e77327fc7e470c773133c766e4fef
Deepanshu Jain <deepanshujain@Deepanshus-MacBook-Pro.local>
2018-11-28 13:47:16 +0530

    cleaned Utils.java

--- android/app/src/main/java/com/vdocipherdemo/Utils.java
+++ android/app/src/main/java/com/vdocipherdemo/Utils.java
@@ -6,23 +6,6 @@ import java.net.InetAddress;
 
 public class Utils {
 
-    public static String getSizeString(int bitsPerSec, long millisec) {
-        double sizeMB = ((double)bitsPerSec / (8 * 1024 * 1024)) * (millisec / 1000);
-        return round(sizeMB, 2) + " MB";
-    }
-
-    public static double round(double value, int places) {
-        if (places < 0) throw new IllegalArgumentException();
-
-        BigDecimal bd = new BigDecimal(value);
-        bd = bd.setScale(places, RoundingMode.HALF_UP);
-        return bd.doubleValue();
-    }
-
-    public static long getSizeBytes(int bitsPerSec, long millisec) {
-        return (bitsPerSec / 8) * (millisec / 1000);
-    }
-
     public static boolean isInternetAvailable() {
         try {
             InetAddress ipAddr = InetAddress.getByName("codingninjas.in");
